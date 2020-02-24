8ff105d3a71c7b2a01990cf281bb98538ae7d1f4
Mac Rae <mac.rae@salesagility.com>
2020-02-24 11:16:00 +0000

    Revert "sort comments"
    This reverts commit 6f6b0d73

--- tests/_support/InstallTester.php
+++ tests/_support/InstallTester.php
@@ -156,33 +156,32 @@ class InstallTester extends Actor
 
     public function dontSeeErrors()
     {
+        printf('- This is the DEBUG output!');
+        printf('- This is the DEBUG output!');
+        printf('- This is the DEBUG output!');
+        printf('- This is the DEBUG output!');
+        printf('- This is the DEBUG output!');
+        printf('- This is the DEBUG output!');
         $I = $this;
-        $I->comment('- This is the DEBUG output!');
-        $I->comment('- This is the DEBUG output!');
-        $I->comment('- This is the DEBUG output!');
-        $I->comment('- This is the DEBUG output!');
-        $I->comment('- This is the DEBUG output!');
-        $I->comment('- This is the DEBUG output!');
-
         if ($I->dontSee('Warning'))
         {
-            $I->comment('1. This is the DEBUG output!');
-            $I->comment($I);
+            printf('1. This is the DEBUG output!');
+            print_r($I);
         }
-        if ($I->dontSee('Notice'))
-        {
-            $I->comment('2. This is the DEBUG output!');
-            $I->comment($I);
-        }
+//        if ($I->dontSee('Notice'))
+//        {
+//            printf('2. This is the DEBUG output!');
+//            print_r($I);
+//        }
         if ($I->dontSee('Error'))
         {
-            $I->comment('3. This is the DEBUG output!');
-            $I->comment($I);
+            printf('3. This is the DEBUG output!');
+            print_r($I);
         }
         if ($I->dontSee('error'))
         {
-            $I->comment('4. This is the DEBUG output!');
-            $I->comment($I);
+            printf('4. This is the DEBUG output!');
+            print_r($I);
         }
     }
 
